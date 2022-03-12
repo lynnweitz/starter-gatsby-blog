3628f74065705592d52f0e836a173c0508ec0096
lynnweitz <81984254+lynnweitz@users.noreply.github.com>
2022-03-12 17:53:34 -0500

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -12,12 +12,12 @@ const Navigation = () => (
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
-          Home
+          Casa
         </Link>
       </li>
       <li className={styles.navigationItem}>
         <Link to="/blog/" activeClassName="active">
-          Blog
+          News
         </Link>
       </li>
     </ul>
